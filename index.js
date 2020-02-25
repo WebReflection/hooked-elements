664537f739d2dc57896c214aa7a30fed554b5c9e
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-02-25 02:56:58 +0100

    updated dependencies

--- index.js
+++ index.js
@@ -420,7 +420,7 @@ var hookedElements = (function (exports) {
     for (var k = keys(definition), i = 0, length = k.length; i < length; i++) {
       var listener = k[i];
 
-      if (/^on/.test(listener)) {
+      if (/^on/.test(listener) && !/Options$/.test(listener)) {
         var options = definition[listener + 'Options'] || false;
         var lower = listener.toLowerCase();
         var type = lower.slice(2);
